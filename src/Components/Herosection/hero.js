4d04e0c9a8dcff0842a61c87aa843675aeb33de0
MaYa Endeshew <46784890+MaYaEndeshew@users.noreply.github.com>
2021-08-05 22:46:35 +0300

    Update hero js
    Added HeroBg closed tag

--- src/Components/Herosection/hero.js
+++ src/Components/Herosection/hero.js
@@ -17,23 +17,24 @@ const Hero = () => {
              <HeroContainer id={"home"} >
             
                 <HeroBg>
-                < video 
-                   autoPlay
-                   loop 
-                   muted
-                   style={{
-                       position:"absolute",
-                       width: "100%",
-                       left: "50%",
-                       top: "50%",
-                       height: "100%",
-                       objectFit: "cover",
-                       transform: "translate(-50%,-50%)",
-                       zIndex: "-1"
-                      
-                    }}>
-                    <source src={Video1} type="video/mp4"/>
-                </video>
+                     < video 
+                        autoPlay
+                        loop 
+                        muted
+                        style={{
+                            position:"absolute",
+                            width: "100%",
+                            left: "50%",
+                            top: "50%",
+                            height: "100%",
+                            objectFit: "cover",
+                            transform: "translate(-50%,-50%)",
+                            zIndex: "-1"
+
+                         }}>
+                         <source src={Video1} type="video/mp4"/>
+                     </video>
+                </HeroBg>
             
                <HeroContent>
                 {/* <button onClick={() => setModalIsOpen(true)}>Get An Estimate</button> */}
